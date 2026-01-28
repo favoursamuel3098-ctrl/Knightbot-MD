0edee2cbaad1b0ab8f816a3893c2f7e668bb3308
Frank Alex 👀🗡️ <favoursamuel3098@gmail.com>
2026-01-28 08:23:11 +0100

    Update bot owner and command mode settings

--- settings.js
+++ settings.js
@@ -2,10 +2,10 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Frank Alex', // Your name
+  ownerNumber: '2348022127030', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "Private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
